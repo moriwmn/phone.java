7d46608db09b2a51c76e8182868f6682b490add2
NoaAlter <105562571+NoaAlter@users.noreply.github.com>
2022-05-15 22:08:59 +0300

    Update CalendarApp.java
    update calender app. added 2 new func-add_meeting and add_event

--- CalendarApp.java
+++ CalendarApp.java
@@ -23,40 +23,70 @@ public class CalendarApp {
     }
 	//methods:
 	
-	public void add_event() {
-		
-		System.out.println("Do you want to create a meeting or event? please enter: M/E");
-		String choice = input.nextLine();
-		String dure;
-		if (choice == "M" || choice == "m")
+	
+	public void add_meeting(string name) {
+		System.out.println("when is your event? pls enter the year:");
+		int year=input.nextLine();
+		System.out.println("pls enter the month:");
+		int month=input.nextLine();
+		System.out.println("pls enter the day:");
+		int day=input.nextLine();
+		System.out.println("pls enter the time of the meeting. in what hour?");
+		int hrs=input.nextLine()
+		System.out.println("how many minutes after"+ hour+ "?");
+		int min=input.nextLine()
+		if(CheckIsValidDate(year,month,day)) 
 		{
-			//AddMeeting()
-			System.out.println("Who will attend the meeting?");
-			choice = input.nextLine();
-			//checking if the person is in the phonebook
-			System.out.println("when is the event?");
-			//create date object
-			System.out.println("for how long?");
-			dure=input.nextLine();
-			//MeetingEvent new_event=new MeetingEvent(date,dure, choice)
+			Date dateOfEvent= new Date(year,month,day,hrs,min);//create date object
+			System.out.println("for how long? (1-60 minutes)");
+			string duretion=input.nextLine();
+			if(duretion<1 || duretion > 60)
+				System.out.println("you are out of range");//TODO error massage
+			MeetingEvent new_event=new MeetingEvent(dateOfEvent,duretion, name)//MeetingEvent(Date date,int duration,String contact
+			this.calendar.add.new_event;/////////////////////
 		}
-		if (choice == "E" || choice == "e")
+		else
+			System.out.println("This date is not valid. pls enter a date that is in 30 days from today :)");//TODO: error massage			
+	}
+
+
+	
+	public void add_event()
+	{
+		System.out.println("when is your event? pls enter the year:");
+		int year=input.nextLine();
+		System.out.println("pls enter the month:");
+		int month=input.nextLine();
+		System.out.println("pls enter the day:");
+		int day=input.nextLine();
+		System.out.println("pls enter the time of the meeting. in what hour?");
+		int hrs=input.nextLine()
+		System.out.println("how many minutes after"+ hour+ "?");
+		int min=input.nextLine()
+		if(CheckIsValidDate(year,month,day)) 
 		{
-			//AddEvent()
-			System.out.println("when is the event?");
-			//create date object
-			System.out.println("for how long?");
-			dure=input.nextLine();
-			System.out.println("please describe the event");
-			choice = input.nextLine();//description
-			//RegEvent new_event=new RegEvent(date,dure, choice)
+			Date dateOfEvent= new Date(year,month,day,hrs,min);//create date object
+			System.out.println("for how long? (1-60 minutes)");
+			string duretion=input.nextLine();
+			if(duretion<1 || duretion > 60)
+				System.out.println("you are out of range");//TODO error massage
+			System.out.println("Add description");
+			string dsc=input.nextLine();
+			RegEvent new_event=new RegEvent(dateOfEvent,duretion, dsc)//MeetingEvent(Date date,int duration,String contact
+			this.calendar.add.new_event;/////////////////////
+		}
+		else
+			System.out.println("This date is not valid. pls enter a date that is in 30 days from today :)");//TODO: error massage			
+	}
+	//RegEvent new_event=new RegEvent(date,dure, choice)
 			//array[index].add(value); - inserting
-		}
+		
 		
 		
 	}
 	
 	public void remove_event() {}
+	
 	public void menu() {
 	System.out.println("********************Menu********************");
 	System.out.println("1. add event ");
