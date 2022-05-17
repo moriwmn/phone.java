dc748b3a1160c368153e6efe60616a99651828a0
moriwmn <105561820+moriwmn@users.noreply.github.com>
2022-05-17 19:17:19 +0300

    update main

--- TestMobilePhone.java
+++ TestMobilePhone.java
@@ -1,1 +1,13 @@
+package ex2;
+
+//Group #9
+//Shira Landau 208275248
+//Noa Kohan 315243501
+//Moriya Weitzman 206607459
+
+
+public static void main(String[] args) {
+	Myphone = new Phone;
+	Myphone.PhoneMenu();
+}
 
