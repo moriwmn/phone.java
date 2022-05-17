09cc8ae1e8adb755b3636a24ecbab6d5a4e8fb45
moriwmn <105561820+moriwmn@users.noreply.github.com>
2022-05-17 21:52:07 +0300

    Update MeetingEvent.java
    debug- change toString

--- MeetingEvent.java
+++ MeetingEvent.java
@@ -1,6 +1,5 @@
 package ex2;
 
-import java.util.Scanner;
 import java.util.Date;
 
 
@@ -22,19 +21,7 @@ public class MeetingEvent extends Event {
 	}
 	@Override
 	public String toString() {
-		return super.toString()+"\r\nwith contact: "+this.getName().toString()+"\r\n";
+		return super.toString()+"\r\nwith contact: "+this.getContact().toString()+"\r\n";
 	}
-	@Override
-	public boolean equals(Object o) {
-		if(o==this) {
-			return true;
-		}
-		if(!(o instanceof MeetingEvent)) {
-			return false;
-		}
-		EventCon e= (MeetingEvent) o;
-		return this.getContact().equals(e.getContact())&& this.getDate().equals(e.getDate()) && this.getDuration()==e.getDuration();
-	}
-
 
 }
